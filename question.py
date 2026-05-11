1655ba9a82c4b53c61b6821361639c8c7b9e99fc
EmilieG139 <2533139@etu.cegepjonquiere.ca>
2026-05-11 08:25:58 -0400

    jagsd

--- question.py
+++ question.py
@@ -1,6 +1,9 @@
+from matplotlib import image
+
+
 class Question:
 
-    def __init__(self, question : str, choix_rep : str, reponse : str ):
+    def __init__(self, question : str, choix_rep : str, reponse : str, fichier_image  ):
         self.question = question
         self.choix_rep = choix_rep
         self.reponse = reponse
